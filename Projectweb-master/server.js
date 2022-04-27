e75369fe2ec7e4e05a2b77aaf0d4d22f7eda6c17
xrl111 <93368873+xrl111@users.noreply.github.com>
2022-04-27 09:41:14 +0700

    sửa

--- Projectweb-master/server.js
+++ Projectweb-master/server.js
@@ -3,7 +3,7 @@ const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
@@ -26,6 +26,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen((process.env.PORT || 5000), () => console.log(`Server started on port ${PORT}`));
